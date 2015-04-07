982bae8ab4620a0841cf83e2273db01d52f38152
himanshu <ho2es@virgina.edu>
2015-04-06 22:37:08 -0400

    user auth complete

--- shopsmart/home/views.py
+++ shopsmart/home/views.py
@@ -2,21 +2,20 @@ from django.shortcuts import render, render_to_response, redirect
 from django.http import HttpResponse
 from home.models import User, user_exists
 from django.template import RequestContext, loader
+from home.custom_shortcuts import render_with_no_context, render_with_context
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
         if user_exists(request.POST['email']):
-            return render("home.html")
+            return render_with_no_context(request, 'home.html')
         else:
            User(email=request.POST['email']).save()
-        return render("home.html")
+        return render_with_no_context(request, 'home.html')
     else:
         return redirect('landing')
 
 
 # Create your views here.
 def landing(request):
-    template = loader.get_template('landing.html')
-    context = RequestContext(request, {})
-    return HttpResponse(template.render(context))
+    return render_with_no_context(request, 'landing.html')
