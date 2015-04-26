0e4af4099cb926782ae50c666436ed95ef029480
himanshu <ho2es@virgina.edu>
2015-04-25 20:50:14 -0400

    merged seperate code

---
+++ shopsmart/home/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('home', '0003_user_products_seen'),
+        ('home', '0002_remove_product_price_discount'),
+    ]
+
+    operations = [
+    ]
